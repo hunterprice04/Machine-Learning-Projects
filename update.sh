1cb6cc464fe9f6b5786420c2d1e6cf11574df653
Hunter Price <hunterprice4044@gmail.com>
2022-05-15 19:47:21 -0400

    added a project in progress

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ git submodule update --init --recursive
 path=$(pwd)
 cat .gitmodules | grep "path" | awk '{print $3}' | while read line
 do
+    echo '###############################################'
     cd $line
     echo "Updating $line"
     git checkout main
