60c1d6a81a1a0b5948b0fd51278110b3b659b5c1
Hunter Price <hunterprice4044@gmail.com>
2022-05-15 19:45:30 -0400

    added script to automatically pull new updates from git on main branch

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git pull
+git submodule update --init --recursive
+path=$(pwd)
+cat .gitmodules | grep "path" | awk '{print $3}' | while read line
+do
+    cd $line
+    echo "Updating $line"
+    git checkout main
+    git pull
+    cd $path
+done
