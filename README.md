c371944f60a103e0678ed361e715ec10a735a973
Hunter Price <hunterprice4044@gmail.com>
2022-05-15 20:08:48 -0400

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Research-Projects
 This repo contains submodules pointing to my research projects for various courses at the University Of Tennessee.
+
+The following are links to the individual projects:
+- [Comparison-of-PSO-and-Gradient-Descent-in-Optimization-For-Neural-Networks](https://github.com/hunterprice04/Comparison-of-PSO-and-Gradient-Descent-in-Optimization-For-Neural-Networks)
+- [Distributed-Deep-Learning-Network-to-Predict-Location-of-Google-Street-View-Data](https://github.com/hunterprice04/Distributed-Deep-Learning-Network-to-Predict-Location-of-Google-Street-View-Data)
+- [Implementation-Of-A-Lightweight-Transformer-And-Analysis-Of-Text-Generation-Sampling-Techniques](https://github.com/hunterprice04/Implementation-Of-A-Lightweight-Transformer-And-Analysis-Of-Text-Generation-Sampling-Techniques)
--- update.sh
+++ update.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
+echo $1
+SHOULD_PUSH=false
+if [[ "$1" == "--push" ]]; then
+    SHOULD_PUSH=true
+fi
+
+echo 'Updating README...'
+echo "# Research-Projects" > README.md
+echo "This repo contains submodules pointing to my research projects for various courses at the University Of Tennessee." >> README.md
+echo "">> README.md
+echo "The following are links to the individual projects:" >> README.md
+
+
 git pull
 git submodule update --init --recursive
 path=$(pwd)
 cat .gitmodules | grep "path" | awk '{print $3}' | while read line
 do
-    echo '###############################################'
     cd $line
-    echo "Updating $line"
+
+    echo '##############################################################################################'
+    echo "Pulling newest commits for: $line"
     git checkout main
     git pull
+
+    echo "Updating README.md"
+    url=`git config --get remote.origin.url | sed 's/git@github\.com:/https:\/\/github\.com\//' | sed 's/\.git//'`
+    echo "URL: $url"
+    echo "- [$line]($url)" >> $path/README.md
     cd $path
 done
+
+if [[ $SHOULD_PUSH == true ]]; then
+    git add .
+    git commit -m "Update README.md"
+    git push
+fi
+echo '##############################################################################################'
